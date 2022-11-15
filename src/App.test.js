b7fc5d3f4bba736221119151f79b9a2479839b05
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2022-11-15 17:23:25 +0100

    add basic tests for all the routes

--- src/App.test.js
+++ src/App.test.js
@@ -92,6 +92,103 @@ describe("if window.innerWidth = 1200", () => {
       expect(buttonNextOnDemeonstration).toBeInTheDocument();
     });
 
-});
+    // verify navigation to /survey/demonstration (same route but a second page) works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnDemeonstration);
+      expect(screen.getByText(/You can have a demonstration page with a single video player (custom size)/)).toBeInTheDocument();
+
+      const buttonNextOnDemeonstration2 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnDemeonstration2).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/demonstration (same route but a third page) works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnBackground2);
+      expect(screen.getByText(/You can have a demonstration page with a single audio player (custom size)/)).toBeInTheDocument();
+
+      const buttonNextOnDemeonstration3 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnDemeonstration3).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case1 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnBackground3);
+      expect(screen.getByText(/Case 1\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase1 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase1).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case2 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase1);
+      expect(screen.getByText(/Case 2\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase2 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase2).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case3 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase2);
+      expect(screen.getByText(/Case 3\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase3 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase3).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case4 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase3);
+      expect(screen.getByText(/Case 4\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase4 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase4).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case5 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase4);
+      expect(screen.getByText(/Case 5\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase5 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase5).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case6 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase5);
+      expect(screen.getByText(/Case 6\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase6 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase6).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/case7 works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase6);
+      expect(screen.getByText(/Case 7\/7/)).toBeInTheDocument();
+
+      const buttonNextOnCase7 = screen.getByRole('button', {name: 'Next'});
+      expect(buttonNextOnCase7).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/summary-and-feedback works and page renders correctly
+    waitFor(() => {
+      user.click(buttonNextOnCase7);
+      expect(screen.getByText(/Summary of cases/)).toBeInTheDocument();
+      expect(screen.getByText(/Overall feedback/)).toBeInTheDocument();
+
+      const buttonEndSurvey = screen.getByRole('button', {name: 'End Survey'});
+      expect(buttonEndSurvey).toBeInTheDocument();
+    });
+
+    // verify navigation to /survey/end works and page renders correctly
+    waitFor(() => {
+      user.click(buttonEndSurvey);
+      expect(screen.getByText(/Thank you for participating in our survey!/)).toBeInTheDocument();
+    });
+
+  });
 
 });
