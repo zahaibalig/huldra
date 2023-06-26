521b04c5d70f6fae56355c8b110a6fc939ce6241
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2023-06-26 16:06:57 +0200

    check if case assets exist with fetch

--- src/utils/loadAssets.js
+++ src/utils/loadAssets.js
@@ -12,32 +12,35 @@ const fetchCases = async (configExists, path, cases, shuffle) => {
   let validCases;
 
   // firebase
-  // validCases= await fetchCasesFirebase(configExists, path, cases, shuffle);
+  validCases = await fetchCasesFirebase(configExists, path, cases, shuffle);
 
   // local
-  validCases = fetchCasesLocal(configExists, path, cases, shuffle);
+  // validCases = fetchCasesLocal(configExists, path, cases, shuffle);
 
   return validCases;
 };
 
 const getAsset = async (path) => {
   const defaults = {
-    "assetsStorageType": "local",
-    "responsesStorageType": "download"
-};
+    assetsStorageType: "local",
+    responsesStorageType: "download",
+  };
 
   // read the configs. if the parameters under "storage" under "REACT_APP_general" are defined, use them; otherwise, use the default values
   const REACT_APP_general = fetchConfigVariable("REACT_APP_general");
   let config = defaults;
   if (REACT_APP_general && REACT_APP_general.storage) {
-    config.assetsStorageType = REACT_APP_general.storage.assetsStorageType || defaults.assetsStorageType;
-    config.responsesStorageType = REACT_APP_general.storage.responsesStorageType || defaults.responsesStorageType;
+    config.assetsStorageType =
+      REACT_APP_general.storage.assetsStorageType || defaults.assetsStorageType;
+    config.responsesStorageType =
+      REACT_APP_general.storage.responsesStorageType ||
+      defaults.responsesStorageType;
   }
 
   let url = getAssetLocal(path);
-  if (!url) {
-    url = await getAssetFirebase(path);
-  }
+  // if (!url) {
+  url = await getAssetFirebase(path);
+  // }
   return url;
 };
 
--- src/utils/loadLocalAssets.js
+++ src/utils/loadLocalAssets.js
@@ -1,34 +1,160 @@
-const fetchCases = () => {
-	const path = "src/assets/gallery/cases/";
+const fetchCases = async () => {
+  // read from config.json
+  const cases = [
+    "image-test",
+    "image-flower",
+    "audio-bird",
+    "video-bird",
+    "hybrid-flowerbird",
+  ];
 
-	const validCases = [
-		"image-flower",
-		"audio-bird",
-		"video-bird",
-		"hybrid-flowerbird",
-	];
+  // loop through the cases and check if the assets with possible extensions exist. stop at the first one that exists
+  const validCases = [];
+  for (let i = 0; i < cases.length; i++) {
+    const isValid = await validateCase(cases[i]);
+    console.log(cases[i], isValid);
+  }
 
-	return validCases;
-}
+  // console.log("validCases  = ", validCases);
 
-const getAsset = (path) => {
-	console.log("getAsset: path = ", path);
+  return cases;
+};
 
-	// if path begins with a slash, remove it
-	if (path[0] === "/") {
-	  path = path.slice(1);
-	}
+const validateCase = async (caseName) => {
+  const path = "/gallery/cases/";
 
-	// try to get the asset from the local assets folder. if it doesn't exist, return null
-	let url = null;
-	try {
-	  url = require(`../assets/${path}`);
-	} catch (err) {
-	  console.log(err);
-	}
-
-	return url;
+  // supported file extensions
+  const extensions = {
+    image: ["jpg", "jpeg", "png", "gif"],
+    audio: ["mp3", "wav", "ogg", "aac", "flac"],
+    video: ["mp4", "webm", "mov"],
   };
+
+  const type = caseName.split("-")[0];
+  const fileNameBase = `${path}${caseName}/${caseName}`;
+
+  if (type === "image") {
+    const fileName1 = `${fileNameBase}.json`;
+    const fileExists1 = await fileExists(fileName1, "json");
+    if (!fileExists1) {
+      return false;
+    }
+
+    const fileNameArrayArray = [
+      extensions.image.map((ext) => `${fileNameBase}.${ext}`),
+      extensions.image.map((ext) => `${fileNameBase}-a.${ext}`),
+      extensions.image.map((ext) => `${fileNameBase}-b.${ext}`),
+    ];
+
+    const groupExists = await fileGroupExists(fileNameArrayArray, "image");
+    return groupExists;
+  } else if (type === "audio") {
+    const fileNameArrayArray = [
+      extensions.audio.map((ext) => `${fileNameBase}-a.${ext}`),
+      extensions.audio.map((ext) => `${fileNameBase}-b.${ext}`),
+    ];
+    const groupExists = await fileGroupExists(fileNameArrayArray, "audio");
+    return groupExists;
+  } else if (type === "video") {
+    const fileNameArrayArray = [
+      extensions.video.map((ext) => `${fileNameBase}-a.${ext}`),
+      extensions.video.map((ext) => `${fileNameBase}-b.${ext}`),
+    ];
+    const groupExists = await fileGroupExists(fileNameArrayArray, "video");
+    return groupExists;
+  } else if (type === "hybrid") {
+    const fileName1ArrayArray = [
+      extensions.video.map((ext) => `${fileNameBase}.${ext}`),
+    ];
+    const group1Exists = await fileGroupExists(fileName1ArrayArray, "video");
+    if (!group1Exists) {
+      return false;
+    }
+
+    const fileName2ArrayArray = [
+      extensions.image.map((ext) => `${fileNameBase}-a.${ext}`),
+      extensions.image.map((ext) => `${fileNameBase}-b.${ext}`),
+    ];
+    const group2Exists = await fileGroupExists(fileName2ArrayArray, "image");
+    return group2Exists;
+  }
+
+  return true;
+};
+
+const fileExists = async (fullPath, fileType) => {
+  try {
+    const response = await fetch(fullPath, { method: "HEAD" });
+
+    // the React app will always return response.ok = true and the index.html, even if the file we request doesn't exist
+    // so we have to check the content-type header to see if the file is the correct type
+    const types = {
+      json: "application/json",
+      image: "image",
+      audio: "audio",
+      video: "video",
+    };
+
+    const contentType = response.headers.get("content-type");
+    if (contentType && contentType.startsWith(types[fileType])) {
+      return true;
+    }
+  } catch (err) {}
+};
+
+// fileNameArrayArray is an array of arrays. the files in a file group have the same file type, but different file names. for each file name, we check if any of the supported file extensions exist. if any of them exist, we return true.
+const fileGroupExists = async (fileNameArrayArray, fileType) => {
+  for (let i = 0; i < fileNameArrayArray.length; i++) {
+    const fileNameArray = fileNameArrayArray[i];
+    let fileNameExists = false;
+    for (let j = 0; j < fileNameArray.length; j++) {
+      const fileName = fileNameArray[j];
+      const exists = await fileExists(fileName, fileType);
+      // console.log(fileName, exists);
+      if (exists) {
+        fileNameExists = true;
+        break;
+      }
+    }
+    if (!fileNameExists) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// for a given file type, this function checks all the supported file extensions to see if any of them exist. will return true on the first one that exists
+// const fileTypeExists = async (fileNameArray, fileType) => {
+//   for (let i = 0; i < fileNameArray.length; i++) {
+//     const fileName = fileNameArray[i];
+//     const exists = await fileExists(fileName, fileType);
+//     if (exists) {
+//       return true;
+//     }
+//   }
+// };
+
+const getAsset = async (path) => {
+  fetchCases();
+  //   const exists = await fileExists("/gallery/countdown.mp4", "video");
+  //   console.log("exists = ", exists);
+  // console.log("getAsset: path = ", path);
+
+  // if path begins with a slash, remove it
+  if (path[0] === "/") {
+    path = path.slice(1);
+  }
+
+  // try to get the asset from the local assets folder. if it doesn't exist, return null
+  let url = null;
+  try {
+    url = require(`/${path}`);
+  } catch (err) {
+    //  console.log(err);
+  }
+
+  return url;
+};
 
 
 export { fetchCases, getAsset };
