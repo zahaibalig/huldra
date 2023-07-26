7cd519fab6114077dda9070b7c2e3873035be9f7
BettyJJ <245282+BettyJJ@users.noreply.github.com>
2023-07-26 12:33:16 +0200

    move HeaderWrapper to its own file

--- src/pages/survey.jsx
+++ src/pages/survey.jsx
@@ -14,9 +14,7 @@ import { toastError, toastInfo } from "../utils/toast";
 import { validateFeedbackForm } from "../utils/inputValidation";
 import version from "../VERSION.md";
 import useHotkeys from "@reecelucas/react-use-hotkeys";
-import { copyToClipboard } from "../utils/text";
 import ProtectedRoute from "../minor-components/protectedRoute";
-import Header from "../minor-components/header";
 import { logSessionEvent } from "../utils/localStorage";
 import Modal from "@mui/material/Modal";
 import ConfirmationDialog from "../minor-components/confirmationDialog";
@@ -27,6 +25,7 @@ import { getButtonProps } from "../utils/survey-utils/getButtonProps";
 import { handlePreviousButton } from "../utils/survey-utils/handlePrevious";
 import { handleNextButton } from "../utils/survey-utils/handleNext";
 import CaseWrapper from "../survey-components/caseWrapper";
+import HeaderWrapper from "../survey-components/headerWrapper";
 
 const Survey = ({
   history,
@@ -254,35 +253,6 @@ const Survey = ({
     disableNextButton,
     REACT_APP_general
   );
-
-  const HeaderWrapper = () => {
-    const pageIsRegistration = history.location.pathname === "/survey/registration";
-    const pageIsEndPage = history.location.pathname === "/survey/end";
-    const pageIsHome = history.location.pathname === "/survey/home";
-
-    if (localStorage.length > 0 && !pageIsRegistration && !pageIsEndPage && !pageIsHome) {
-      return (
-        <Header
-          leftLabel={`Participant ID: ${
-            JSON.parse(localStorage.getItem("ParticipantInfo"))["ParticipantId"]
-          }`}
-          leftIcon1TooltipMessage="This is your participant ID. You can copy this ID to keep for later reference, as well as to be able to resume your survey in case of accidental exit before completion."
-          leftIcon2TooltipMessage=" Copy to clipboard"
-          leftIcon1ClassName="fa fa-info-circle form-tooltip"
-          leftIcon2ClassName="fa fa-clone ml-3 form-tooltip"
-          leftIcon1OnClick={() => {
-            return;
-          }}
-          leftIcon2OnClick={() =>
-            copyToClipboard(JSON.parse(localStorage.getItem("ParticipantInfo"))["ParticipantId"])
-          }
-          history={history}
-        />
-      );
-    } else {
-      return null;
-    }
-  };
 
   return (
     <div
